523ffab46a3247f432527a55824fbb8465a09a99
Jaenal Lee <gloryvibes@Jae.local>
2018-08-28 11:06:27 +0700

    setup mola mockup

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => (
+    <div className={header__container}>
+        <div>MOLA</div>
+        <div>CATEGORY</div>
+        <div>MENU</div>
+    </div>
+);
+
+export default Header;
