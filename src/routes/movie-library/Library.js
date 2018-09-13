74e0071ab4229654b9129a342dfda40d34d7042a
Donny Riantori <odonplay@gmail.com>
2018-09-13 18:16:37 +0700

    movie library
    1. update header navigation
    2. update mobile version

--- src/routes/movie-library/Library.js
+++ src/routes/movie-library/Library.js
@@ -2,127 +2,122 @@ import React, { Fragment, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import CardLibrary from './movielibrary/Card'
 import Layout from '../../components/Molalayout';
-import Masonry from 'react-masonry-component';
 // import Header from '../../components/header';
 // import Navbar from '../../components/navigation';
 
 import Libheader from './movielibrary/Libheader';
 import s from './Library.css';
 
-const masonryOptions = {
-  transitionDuration: 0
-};
-
 class MovieLibrary extends Component {
   state = {
     cardLink: 'http://google.com',
+    title: 'ACTION',
   }
 
   cardLibImg = () => [
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x400/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x400/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x400/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x400/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x400/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x400/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x400/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x400/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
-    },{
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x400/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x400/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x400/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x400/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x400/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x400/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x400/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x400/000/fff',
     },
     {
-      cardImageLib: 'https://dummyimage.com/220x138/000/fff',
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
     },
+    {
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
+    },
+    {
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
+    },
+    {
+      cardImageLib: 'https://dummyimage.com/266x138/000/fff',
+    },
+
   ];
   render() {
-    const { cardLink } = this.state;
+    const { cardLink, title } = this.state;
     return (
       <Fragment>
         <Layout>
           <div className={s.main_container}>
-            <Libheader />
+            <Libheader cardTitle={title}/>
             <div className={s.card_wrapper}>
-              <Masonry
-                className={'my_gallery_class'} // default ''
-                elementType={'ul'} // default 'div'
-                options={masonryOptions} // default {}
-                disableImagesLoaded={false} // default false
-                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-                // imagesLoadedOptions={imagesLoadedOptions} // default {}
-              >
-                {this.cardLibImg().map(obj => (
-                  <CardLibrary key={obj.toString()} imgUrl={obj.cardImageLib} cardLink={cardLink}/>
-                ))}
-              </Masonry>
+              {this.cardLibImg().map(obj => (
+                <CardLibrary key={obj.toString()} imgUrl={obj.cardImageLib} cardLink={cardLink}/>
+              ))}
             </div>
           </div>
         </Layout>
