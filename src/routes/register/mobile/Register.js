43106ca0d64003f03681cd77412e8a63b70d4b5f
regi saputra <regisaputra@regis-MacBook-Pro.local>
2018-09-24 19:32:35 +0700

    hidden style password

--- src/routes/register/mobile/Register.js
+++ src/routes/register/mobile/Register.js
@@ -16,71 +16,109 @@ import Form from '@components/FormInput';
 import facebook from '@global/style/icons/facebook.png';
 import google from '@global/style/icons/google.png';
 import line from '@global/style/icons/line.png';
+import LazyLoad from '@components/common/LazyLoad';
 
 class Register extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: '',
+      emailPhone: '',
+      password: '',
+      confirmPassword: ''
+    }
+
+    this.onChangeInput = this.onChangeInput.bind(this)
+  }
+
+  onChangeInput = (e) => {
+    const target = e.target
+    const { id, value } = target
+    this.setState({
+      [id]: value
+    })
+  }
+
   static propTypes = {
     title: PropTypes.string.isRequired,
   };
 
   render() {
+    const {
+      username,
+      emailPhone,
+      password,
+      confirmPassword
+    } = this.state
+
     const isDark = true;
     return (
       <Fragment>
         <Header isDark={isDark} libraryOff rightMenuOff isMobile />
         <div className={s.wrapper}>
           <div className={s.root}>
-            <div className={s.container}>
-              <p className={s.labelHeader}>Daftar mola sekarang !</p>
-              <form method="post">
-                <Form className={s.formMobile}
-                  id="username"
-                  type="text"
-                  name="username"
-                  autoFocus>Username
-                </Form>
-                <Form className={s.formMobile}
-                  id="emailPhone"
-                  type="text"
-                  name="emailPhone"
-                >Email or Phonenumber
-                </Form>
-                <Form className={s.formMobile}
-                  id="password"
-                  type="password"
-                  name="password"
-                >Password
-                </Form>
-                <Form className={s.formMobile}
-                  id="confirmPassword"
-                  type="password"
-                  name="confirmPassword"
-                >Confirm password
-                </Form>
-                <div className={s.formGroup} style={{ marginTop: '15px' }}>
-                  <button className={s.button} type="submit">
+            <LazyLoad>
+              <div className={s.container}>
+                <p className={s.labelHeader}>Daftar mola sekarang !</p>
+                <form method="post">
+                  <Form className={s.formMobile}
+                    id="username"
+                    type="text"
+                    name="username"
+                    onChange={this.onChangeInput}
+                    value={username}
+                    autoFocus>Username
+                  </Form>
+                  <Form className={s.formMobile}
+                    id="emailPhone"
+                    type="text"
+                    name="emailPhone"
+                    onChange={this.onChangeInput}
+                    value={emailPhone}
+                  >Email or Phonenumber
+                  </Form>
+                  <Form className={s.formMobile}
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={this.onChangeInput}
+                    value={password}
+                  >Password
+                  </Form>
+                  <Form className={s.formMobile}
+                    id="confirmPassword"
+                    type="password"
+                    name="confirmPassword"
+                    onChange={this.onChangeInput}
+                    value={confirmPassword}
+                  >Confirm password
+                  </Form>
+                  <div className={s.formGroup} style={{ marginTop: '15px' }}>
+                    <button className={s.button} type="submit">
                                     SIGN UP
-                  </button>
-                </div>
-              </form>
-              <strong className={s.lineThrough}>Atau</strong>
-              <div className={s.flexButton}>
-                <div>
-                  <a className={s.google} href="/login/facebook">
-                    <img src={google} className={s.buttonMobile} />
-                  </a>
-                </div>
-                <div>
-                  <a className={s.facebook} href="/login/facebook">
-                    <img src={facebook} className={s.buttonMobile} />
-                  </a>
-                </div>
-                <div>
-                  <a className={s.line} href="/login/facebook">
-                    <img src={line} className={s.buttonMobile} />
-                  </a>
+                    </button>
+                  </div>
+                </form>
+                <strong className={s.lineThrough}>Atau</strong>
+                <div className={s.flexButton}>
+                  <div>
+                    <a className={s.google} href="/login/facebook">
+                      <img src={google} className={s.buttonMobile} />
+                    </a>
+                  </div>
+                  <div>
+                    <a className={s.facebook} href="/login/facebook">
+                      <img src={facebook} className={s.buttonMobile} />
+                    </a>
+                  </div>
+                  <div>
+                    <a className={s.line} href="/login/facebook">
+                      <img src={line} className={s.buttonMobile} />
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
+            </LazyLoad>
           </div>
         </div>
       </Fragment>
