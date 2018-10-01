02f2b4c738e8c5e15c8510e2e750490265593459
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2018-10-01 11:45:37 +0700

    change config env with REACT_APP_ENV

--- src/lib/request.js
+++ src/lib/request.js
@@ -1,74 +1,77 @@
-const axios = require('axios')
-const apiConfig = require('../global/config/api')
+const axios = require('axios');
+const apiConfig = require('../global/config/api');
 
-const apiHost = `${apiConfig[process.env.NODE_ENV].endpoints.molatv}`;
+const apiHost = `${apiConfig[process.env.REACT_APP_ENV].endpoints.molatv}`;
 module.exports = {
-  get (endPoint, token) {
+  get(endPoint, token) {
     return new Promise((resolve, reject) => {
-      let headers = {}
+      let headers = {};
       if (token) {
-        headers.Authorization = `Bearer ${token}`
+        headers.Authorization = `Bearer ${token}`;
       }
 
-      axios.get(`${apiHost}/${endPoint}`, {
-        headers: headers
-      })
+      axios
+        .get(`${apiHost}/${endPoint}`, {
+          headers: headers
+        })
         .then(response => {
-          var data = response.data
+          var data = response.data;
           if (typeof data.data !== 'undefined') {
-            data = data.data
+            data = data.data;
           }
-          resolve(data)
+          resolve(data);
         })
         .catch(err => {
-          reject(err)
-        })
-    })
+          reject(err);
+        });
+    });
   },
-  post (endPoint, token, payload) {
+  post(endPoint, token, payload) {
     return new Promise((resolve, reject) => {
-      let headers = {}
+      let headers = {};
       if (token) {
-        headers.Authorization = `Bearer ${token}`
+        headers.Authorization = `Bearer ${token}`;
       }
-      headers.ContentType = 'application/json'
+      headers.ContentType = 'application/json';
 
-      axios.post(`${apiHost}/${endPoint}`, payload, {
-        headers: headers
-      })
+      axios
+        .post(`${apiHost}/${endPoint}`, payload, {
+          headers: headers
+        })
         .then(response => {
-          var data = response.data
+          var data = response.data;
           if (typeof data.data !== 'undefined') {
-            data = data.data
+            data = data.data;
           }
-          resolve(data)
+          resolve(data);
         })
         .catch(err => {
-          reject(err)
-        })
-    })
+          reject(err);
+        });
+    });
   },
-  put (endPoint, token, payload) {
+  put(endPoint, token, payload) {
     return new Promise((resolve, reject) => {
-      let headers = {}
+      let headers = {};
       if (token) {
-        headers.Authorization = `Bearer ${token}`
+        headers.Authorization = `Bearer ${token}`;
       }
-      headers.ContentType = 'application/json'
+      headers.ContentType = 'application/json';
 
-      axios.put(`${apiHost}/${endPoint}`, payload, {
-        headers: headers
-      })
+      axios
+        .put(`${apiHost}/${endPoint}`, payload, {
+          headers: headers
+        })
         .then(response => {
-          var data = response.data
+          var data = response.data;
           if (typeof data.data !== 'undefined') {
-            data = data.data
+            data = data.data;
           }
-          resolve(data)
+          resolve(data);
         })
         .catch(err => {
-          reject(err)
-        })
-    })
+          reject(err);
+        });
+    });
   }
-}
+};
