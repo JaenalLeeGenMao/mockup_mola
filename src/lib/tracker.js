80041b8238e747a55816927ed5657c17965f0393
Kareem <kareem.lukitomo@gmail.com>
2018-10-02 15:19:55 +0700

    ⬆️ Add tracking for event_pages. 🔨 WIP video_plays.

---
+++ src/lib/tracker.js
@@ -0,0 +1,109 @@
+import axios from 'axios';
+import _get from 'lodash/get';
+
+const production = process.env.NODE_ENV === 'production';
+const cookieName = production ? '_pst' : '_pst_staging';
+
+const TOKEN_ENDPOINT = production
+  ? 'https://api-g.supersoccer.tv/v1/videos/pubsub'
+  : 'https://api.supersabar.com/v1/videos/pubsub';
+
+const PUBSUB_ENDPOINT = production
+  ? 'https://pubsub.googleapis.com/v1/projects/supersoccer-173813/topics/ds-feeder-guardian:publish'
+  : 'https://pubsub.googleapis.com/v1/projects/staging-199507/topics/ds-feeder-guardian:publish';
+
+class Tracker {
+  static sessionId = () => {
+    let sessionId = _get(document, 'cookie', '')
+      .trim()
+      .split(';')
+      .filter(function(item) {
+        return item.indexOf('__sessId=') >= 0;
+      });
+    if (sessionId.length) {
+      sessionId = sessionId[0].split('=')[1];
+    }
+
+    if (!sessionId) {
+      sessionId =
+        Math.random()
+          .toString(36)
+          .substring(2, 15) +
+        Math.random()
+          .toString(36)
+          .substring(2, 15) +
+        Math.random()
+          .toString(36)
+          .substring(2, 15);
+      document.cookie = `__sessId=${sessionId}; max-age=${60 * 60 * 24}; path=/;`;
+    }
+    return sessionId;
+  };
+
+  static getOrCreateToken = async () => {
+    // let token = window.Cookies.get(cookieName);
+    let token = _get(document, 'cookie')
+      .trim()
+      .split(';')
+      .filter(function(item) {
+        return item.indexOf(`${cookieName}=`) >= 0;
+      });
+    if (token.length > 0) {
+      token = token[0].split('=')[1];
+    } else {
+      token = null;
+    }
+
+    if (!token) {
+      token = await axios
+        .post(TOKEN_ENDPOINT)
+        .then(response => response.data)
+        .then(data => {
+          token = data.access_token;
+          document.cookie = `${cookieName}=${token}; max-age=${60 * 60}; path=/;`;
+          return token;
+        })
+        .catch(() => null);
+    }
+    return token;
+  };
+
+  static sendPubSub = async (payload: Object, token: String) => {
+    const keys = Object.keys(payload.data);
+    const adjustedPayload = payload;
+
+    // Delete/unsend all missing/null values in payload.data
+    keys.forEach(key => {
+      if (
+        !payload.data[key] ||
+        payload.data[key] === null ||
+        payload.data[key].length < 1 ||
+        payload.data[key][0] === null
+      ) {
+        delete adjustedPayload.data[key];
+      }
+    });
+
+    const postToDS = new Buffer(JSON.stringify(payload)).toString('base64');
+    const reqBody = {
+      messages: [
+        {
+          data: postToDS
+        }
+      ]
+    };
+
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify(reqBody),
+      url: PUBSUB_ENDPOINT
+    };
+    return axios(options).catch(() => {});
+  };
+}
+
+export default Tracker;
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,17 @@
 
 /* eslint-disable global-require */
 
+import dateFormat from 'dateformat';
+import _get from 'lodash/get';
+import queryString from 'query-string';
+import UaParser from 'ua-parser-js';
+import history from '../history';
+
+import tracker from '../lib/tracker';
+
+let firstRender = true;
+let currentLocation = history.location;
+
 // The top-level (parent) route
 const routes = {
   path: '',
@@ -17,31 +28,31 @@ const routes = {
   children: [
     {
       path: '',
-      load: () => import(/* webpackChunkName: 'home' */ './home'),
+      load: () => import(/* webpackChunkName: 'home' */ './home')
     },
     {
       path: '/contact',
-      load: () => import(/* webpackChunkName: 'contact' */ './contact'),
+      load: () => import(/* webpackChunkName: 'contact' */ './contact')
     },
     {
       path: '/accounts/login',
-      load: () => import(/* webpackChunkName: 'accounts-login' */ './login'),
+      load: () => import(/* webpackChunkName: 'accounts-login' */ './login')
     },
     {
       path: '/register',
-      load: () => import(/* webpackChunkName: 'register' */ './register'),
+      load: () => import(/* webpackChunkName: 'register' */ './register')
     },
     {
       path: '/about',
-      load: () => import(/* webpackChunkName: 'about' */ './about'),
+      load: () => import(/* webpackChunkName: 'about' */ './about')
     },
     {
       path: '/privacy',
-      load: () => import(/* webpackChunkName: 'privacy' */ './privacy'),
+      load: () => import(/* webpackChunkName: 'privacy' */ './privacy')
     },
     {
       path: '/admin',
-      load: () => import(/* webpackChunkName: 'admin' */ './admin'),
+      load: () => import(/* webpackChunkName: 'admin' */ './admin')
     },
     // Movie details
     {
@@ -49,7 +60,7 @@ const routes = {
       load: () => import(/* webpackChunkName: 'admin' */ './movie-detail'),
       children: [
         {
-          path: '/:id',
+          path: '/:id'
         }
       ]
     },
@@ -59,22 +70,22 @@ const routes = {
       load: () => import(/* webpackChunkName: 'admin' */ './movie-library'),
       children: [
         {
-          path: '/:id',
+          path: '/:id'
         }
       ]
     },
     // Wildcard routes, e.g. { path: '(.*)', ... } (must go last)
     {
       path: '/history',
-      load: () => import(/* webpackChunkName: 'history' */ './history'),
+      load: () => import(/* webpackChunkName: 'history' */ './history')
     },
     {
       path: '/search',
-      load: () => import(/* webpackChunkName: 'search' */ './search'),
+      load: () => import(/* webpackChunkName: 'search' */ './search')
     },
     {
       path: '/accounts/profile',
-      load: () => import(/* webpackChunkName: 'accounts-profile' */ './profile'),
+      load: () => import(/* webpackChunkName: 'accounts-profile' */ './profile')
     },
     // Movie-player
     {
@@ -82,34 +93,36 @@ const routes = {
       load: () => import(/* webpackChunkName: 'movie-player' */ './movie-player'),
       children: [
         {
-          path: '/:id',
+          path: '/:id'
         }
       ]
     },
     {
       path: '/forgotPassword',
-      load: () => import(/* webpackChunkName: 'forgotPassword' */ './forgotPassword'),
+      load: () => import(/* webpackChunkName: 'forgotPassword' */ './forgotPassword')
     },
     {
       path: '/resetPassword',
-      load: () => import(/* webpackChunkName: 'resetPassword' */ './resetPassword'),
+      load: () => import(/* webpackChunkName: 'resetPassword' */ './resetPassword')
     },
     {
       path: '/accounts/security',
-      load: () => import(/* webpackChunkName: 'accounts-security' */ './security'),
+      load: () => import(/* webpackChunkName: 'accounts-security' */ './security')
     },
     {
       path: '/accounts/setting',
-      load: () => import(/* webpackChunkName: 'accounts-setting' */ './setting'),
+      load: () => import(/* webpackChunkName: 'accounts-setting' */ './setting')
     },
     // Wildcard routes, e.g. { path: '(.*)', ... } (must go last)
     {
       path: '(.*)',
-      load: () => import(/* webpackChunkName: 'not-found' */ './not-found'),
-    },
+      load: () => import(/* webpackChunkName: 'not-found' */ './not-found')
+    }
   ],
 
-  async action({ next }) {
+  async action({ next, store }) {
+    track(store);
+
     // Execute each child route until one of them return the result
     const route = await next();
 
@@ -117,14 +130,99 @@ const routes = {
     route.title = `${route.title || 'Untitled Page'}`;
     route.description = route.description || '';
     return route;
-  },
+  }
+};
+
+const track = async store => {
+  if (process.env.BROWSER) {
+    const { location } = window;
+    // Parse Current URL
+    const { search, pathname } = window.location;
+    const urlParams = queryString.parse(search);
+
+    // Get & Parse UA
+    const UA = new UaParser();
+    UA.setUA(navigator.userAgent);
+
+    // Try get user_id & subs
+    const users = _get(store.getState(), 'data.users', {});
+
+    let userId;
+
+    let adjustedSubs = [];
+    // if (userId !== null || userId !== undefined) {
+    //   adjustedSubs = await users.subscriptions.map(
+    //     subs => `${_get(subs, 'subscriptionId', null)}`,
+    //   );
+    // } else {
+    //   adjustedSubs = [null];
+    // }
+
+    // Try get platform and browser
+    const platform = _get(UA.getDevice(), 'type', null);
+    const osName = _get(UA.getOS(), 'name', null);
+    const osVersion = _get(UA.getOS(), 'version', null);
+    const os = osName !== null && osVersion !== null ? `${osName} ${osVersion}` : null;
+    const vendor = _get(UA.getDevice(), 'vendor', null);
+    const mobile = _get(UA.getDevice(), 'mobile', null);
+    const device = vendor !== null && mobile !== null ? `${vendor} ${mobile}` : null;
+
+    const browserName = _get(UA.getBrowser(), 'name', null);
+    const browserVersion = _get(UA.getBrowser(), 'version', null);
+    const browser =
+      browserName !== null && browserVersion !== null ? `${browserName} ${browserVersion}` : null;
+
+    // Initialize Payload
+    const payload = {
+      data: {
+        project_id: 'molatv',
+        referrer: `${window.location.host}${currentLocation.pathname}${currentLocation.search}`,
+        host: `${window.location.host}`,
+        path: `${window.location.host}${location.pathname}${location.search}`,
+        session_id: tracker.sessionId(window), // Try get+set session_id
+        // page_content: document.title || null,
+        ip: _get(store.getState(), 'runtime.clientIp', null),
+        platform,
+        os,
+        device,
+        app: browser,
+        client: 'mola-web',
+        screen_resolution: `${window.screen.width}x${window.screen.height}`,
+        user_id: userId,
+        current_subscription_id: adjustedSubs,
+        hit_timestamp: dateFormat(new Date(), 'yyyy-mm-dd hh:MM:ss')
+      },
+      table: 'event_pages'
+    };
+
+    // if (isChannel || pathname === '/') {
+    //   payload.data.channel = urlParams.v || 'sstv';
+    //   payload.table = 'event_videos';
+    // } else if (isVideo) {
+    //   payload.data.video_id = urlParams.v;
+    //   payload.table = 'event_videos';
+    // }
+
+    // if (firstRender) {
+    //   firstRender = false;
+    //   const referrer = _get(store.getState(), 'runtime.referer', null)
+    //   payload.data.referrer = referrer;
+    // }
+
+    const token = await tracker.getOrCreateToken();
+    // Post to ds-feeder if there's token
+    if (token) tracker.sendPubSub(payload, token);
+
+    // HOTFIX need to find a way to get refferer in react
+    currentLocation = location;
+  }
 };
 
 // The error page is available by permanent url for development mode
 if (__DEV__) {
   routes.children.unshift({
     path: '/error',
-    action: require('./error').default,
+    action: require('./error').default
   });
 }
 
