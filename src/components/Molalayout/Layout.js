ba5fb3db8a87d9694465b5572c57103e065ff748
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2018-10-10 13:02:22 +0700

    add layout css back in mola layout

--- src/components/Molalayout/Layout.js
+++ src/components/Molalayout/Layout.js
@@ -4,7 +4,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // external-global styles must be imported in your JS.
 import normalizeCss from 'normalize.css';
-import styles from './normaliseCss';
+import styles from './Layout.css';
 
 class Layout extends Component {
   static propTypes = {
