ce6439bfc15db263fadcefb6639decc2af06b82d
Donny Riantori <donny.riantori@supersoccer.co.id>
2018-10-15 13:17:46 +0700

    mola mobile
    revisi theoplayer

--- src/routes/movie-detail/mobile/Testimoni.js
+++ src/routes/movie-detail/mobile/Testimoni.js
@@ -6,25 +6,31 @@ import s from './Testimoni.css';
 class Testimoni extends Component {
   static propTypes = {
     testimoniContent: PropTypes.string.isRequired,
-    trailerTitle: PropTypes.string.isRequired,
     testimoniSource: PropTypes.string.isRequired,
-    trailerText: PropTypes.bool.isRequired,
-  }
+    trailerTitle: PropTypes.string,
+    trailerText: PropTypes.bool
+  };
 
   render() {
     return (
       <div className={s.container}>
         <div className={s.box}>
           <div className={s.inner_box}>
-            <p>AAAA{this.props.testimoniContent}</p>
-            <span>AAA{this.props.testimoniSource}</span>
+            <p>
+              AAAA
+              {this.props.testimoniContent}
+            </p>
+            <span>
+              AAA
+              {this.props.testimoniSource}
+            </span>
           </div>
           <div className={s.photo_frame}>
-            <img
-              src={this.props.testimoniPhotoUrl}
-            />
+            <img src={this.props.testimoniPhotoUrl} />
           </div>
-          {this.props.trailerText && <div className={s.trailer_title}>{this.props.trailerTitle}</div>}
+          {this.props.trailerText && (
+            <div className={s.trailer_title}>{this.props.trailerTitle}</div>
+          )}
         </div>
       </div>
     );
