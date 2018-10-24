b08241d187665e867e1c5e6d334f92c644feceac
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2018-10-24 09:45:23 +0700

    change linter config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,30 +1,7 @@
 module.exports = {
-  extends: ['standard', 'standard-react'],
+  extends: ['airbnb', 'prettier'],
   rules: {
-    // Opinionated rule, you can disable it if you want
-    'string-quotes': 'single',
-
-    'property-no-unknown': [
-      true,
-      {
-        ignoreProperties: [
-          // CSS Modules composition
-          // https://github.com/css-modules/css-modules#composition
-          'composes'
-        ]
-      }
-    ],
-
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        ignorePseudoClasses: [
-          // CSS Modules :global scope
-          // https://github.com/css-modules/css-modules#exceptions
-          'global',
-          'local'
-        ]
-      }
-    ]
+    indent: ['error', 4],
+    quotes: [2, 'backtick', 'prefer-single']
   }
 };
