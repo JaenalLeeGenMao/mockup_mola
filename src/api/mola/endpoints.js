415bf8c311c063abfdb315effc261f1181d44dd3
fujianto <septianfujianto@gmail.com>
2018-11-06 12:33:10 +0700

    Remove local dev

--- src/api/mola/endpoints.js
+++ src/api/mola/endpoints.js
@@ -1,18 +1,18 @@
 /* eslint-disable import/prefer-default-export */
 import { endpoints } from '@source/config';
 
-export const HOME_PLAYLIST_ENDPOINT = `http://localhost:3099/${endpoints.api}/videos/playlists`;
+export const HOME_PLAYLIST_ENDPOINT = `${endpoints.api}/videos/playlists`;
 
-export const VIDEOS_ENDPOINT = `http://localhost:3099/${endpoints.api}/videos`;
+export const VIDEOS_ENDPOINT = `${endpoints.api}/videos`;
 // export const VIDEOS_ENDPOINT = 'https://private-86dd3-movie178.apiary-mock.com/videos';
-export const HISTORY_ENDPOINT = `http://localhost:3099/${endpoints.api}/userdata`;
+export const HISTORY_ENDPOINT = `${endpoints.api}/userdata`;
 
-export const SEARCH_ENDPOINT = `http://localhost:3099/${endpoints.api}/search/`;
-export const SEARCH_GENRE_ENDPOINT = `http://localhost:3099/${endpoints.api}/videos/playlists/genre`;
-export const RECENT_SEARCH_ENDPOINT = `http://localhost:3099/${SEARCH_ENDPOINT}histories`;
+export const SEARCH_ENDPOINT = `${endpoints.api}/search/`;
+export const SEARCH_GENRE_ENDPOINT = `${endpoints.api}/videos/playlists/genre`;
+export const RECENT_SEARCH_ENDPOINT = `${SEARCH_ENDPOINT}histories`;
 
-// export const MOVIE_DETAIL_ENDPOINT = `${endpoints.api}/videos`;
-// export const MOVIE_STREAMING = `${endpoints.api}/videos`;
+export const MOVIE_DETAIL_ENDPOINT = `${endpoints.api}/videos`;
+export const MOVIE_STREAMING = `${endpoints.api}/videos`;
 
-export const MOVIE_DETAIL_ENDPOINT = 'https://private-e3227-molawebapi4.apiary-mock.com/videos';
-export const MOVIE_STREAMING = 'https://private-e3227-molawebapi4.apiary-mock.com/videos';
+// export const MOVIE_DETAIL_ENDPOINT = 'https://private-e3227-molawebapi4.apiary-mock.com/videos';
+// export const MOVIE_STREAMING = 'https://private-e3227-molawebapi4.apiary-mock.com/videos';
