e7df5633f59b4ea341ed367f31c78a796245e88b
fujianto <septianfujianto@gmail.com>
2018-12-11 18:11:23 +0700

    rename header

--- src/routes/inbox/mobile/Inbox.js
+++ src/routes/inbox/mobile/Inbox.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import styles from './inbox.css'
+import styles from './Inbox.css'
 import { compose } from 'redux'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 
