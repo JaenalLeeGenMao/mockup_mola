d6331c624216e9f44c490f4b70c608354ecc8ecb
fujianto <septianfujianto@gmail.com>
2019-01-29 17:17:48 +0700

    Implement Open Graph title, image

--- src/routes/movie-detail/index.js
+++ src/routes/movie-detail/index.js
@@ -12,7 +12,9 @@ const description = 'Movie Details Casters Writers Directors and Sponsors'
 async function action({ isMobile, pathname }) {
   const pathnameArr = pathname.split('/')
   const movieId = pathnameArr[2]
-  const { data: [{ title, images: { cover: { thumbnail } } }] } = await Mola.getMovieDetail({ id: movieId })
+  const movieDetail = await Mola.getMovieDetail({ id: movieId })
+  const title = movieDetail.data[0].title
+  const thumbnail = movieDetail.data[0].images.cover.background.mobile.portrait
 
   return {
     chunks: ['movie-detail'],
@@ -21,11 +23,11 @@ async function action({ isMobile, pathname }) {
     description,
     component: isMobile ? (
       <MolaLayout>
-        <MovieDetailMobile pathLoc={pathnameArr[1]} movieId={pathnameArr[2]} />
+        <MovieDetailMobile pathLoc={pathnameArr[1]} movieId={pathnameArr[2]} movieData={movieDetail} />
       </MolaLayout>
     ) : (
       <MolaLayout>
-        <MovieDetailDesktop pathLoc={pathnameArr[1]} movieId={pathnameArr[2]} />
+        <MovieDetailDesktop pathLoc={pathnameArr[1]} movieId={pathnameArr[2]} movieData={movieDetail} />
       </MolaLayout>
     ),
   }
