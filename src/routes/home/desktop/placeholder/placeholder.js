5c7da581b5cb7ac4171369b264ef76cf1049354b
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2019-02-10 17:45:10 +0700

    refactor desktop homepage design, fix swiping scrolling bugs

--- src/routes/home/desktop/placeholder/placeholder.js
+++ src/routes/home/desktop/placeholder/placeholder.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import styles from './placeholder.css';
+import React from 'react'
+import withStyles from 'isomorphic-style-loader/lib/withStyles'
+
+import styles from './placeholder.css'
 
 const Placeholder = () => {
   return (
     <div className={styles.placeholder__container}>
       <div className={styles.placeholder__side_menu}>
+        <div className={styles.placeholder__long_dot} />
         <div className={styles.placeholder__dot} />
         <div className={styles.placeholder__dot} />
         <div className={styles.placeholder__dot} />
       </div>
       <div className={styles.placeholder__detail}>
-        <div className={styles.placeholder__title} />
         <div className={styles.placeholder__line} />
         <div className={styles.placeholder__line} />
         <div className={styles.placeholder__line} />
         <div className={styles.placeholder__line} />
-        <div className={styles.placeholder__button} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
       </div>
-      {/* <div className={styles.placeholder__figure}>{[...Array(100)].map((x, i) => <div key={i} className={styles.placeholder__figure_line} style={{ width: `${Math.random() * 25}%` }} />)}</div> */}
-      <div className={styles.placeholder__arrows}>
-        <div className={styles.placeholder__arrow_prev} />
-        <div className={styles.placeholder__arrow_next} />
+      <div className={styles.placeholder__footer}>
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__line} />
+        <div className={styles.placeholder__footer_arrow}>
+          <div className={styles.placeholder__line} />
+          <div className={styles.placeholder__line} />
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default withStyles(styles)(Placeholder);
+export default withStyles(styles)(Placeholder)
--- src/routes/home/olddesktop/const.js
+++ src/routes/home/olddesktop/const.js
@@ -6,52 +6,49 @@ export const tourSteps = {
     {
       target: '.tourCategory',
       title: 'Kategori Film',
-      content: (
-        <div>
-          Untuk bernavigasi di sekitar kategori film yang berbeda, Anda cukup mengklik tombol navigasi atau geser <span className={styles.swipeUpIcon} /> atas dan ke bawah pada keyboard Anda yang luar
-          biasa
-        </div>
-      ),
+      content: `
+      Untuk bernavigasi di sekitar kategori film yang berbeda, Anda cukup mengklik navigasi
+      tombol atau tekan ↑ atas dan ↓ pada keyboard Anda yang luar biasa
+      `,
       placement: 'right',
       disableBeacon: true,
-      styles: {
-        tooltip: {
-          maxWidth: '100%',
-        },
-      },
+      disableOverlayClicks: true,
     },
     {
       target: '.tourSlide',
       title: 'Film Menarik',
-      content: (
-        <div>
-          {' '}
-          Anda dapat menelusuri film-film top kami di setiap kategori dengan klik lembut pada tombol panah atau menggunakan keyboard dan geser <span className={styles.swipeNextIcon} /> kanan dan kiri
-        </div>
-      ),
+      content: `
+      Anda dapat menelusuri film-film top kami di setiap kategori dengan klik lembut pada tombol panah
+       atau menggunakan keyboard dan beralih → kanan dan ← kiri`,
       placement: 'top',
       disableBeacon: true,
+      disableOverlayClicks: true,
     },
     {
       target: '.tourLibrary',
       title: 'Perpustakaan Film',
-      content: 'Anda dapat menekan ikon ini untuk melihat semua daftar film per kategori',
+      content: 'Anda dapat mengklik ikon ini untuk melihat semua daftar film per kategori',
       placement: 'bottom',
       disableBeacon: true,
+      disableOverlayClicks: true,
     },
     // {
     //   target: '.tourMovieDiscover',
     //   title: 'Temukan Film Kami',
     //   content: 'Klik tombol ini untuk menemukan daftar film kami yang luar biasa',
     //   placement: 'top',
+    //   spotlightPadding: 0,
     //   disableBeacon: true,
+    //   disableOverlayClicks: true,
     // },
     {
       target: '.tourMovieDetail',
       title: 'Lihat Detail Film',
       content: 'Klik tombol ini untuk menonton film dan melihat detail film: sinopsis, testimonial, pemeran, dan cuplikan',
       placement: 'top',
+      spotlightPadding: 0,
       disableBeacon: true,
+      disableOverlayClicks: true,
       locale: { last: 'Selesai' },
     },
   ],
@@ -59,30 +56,22 @@ export const tourSteps = {
     {
       target: '.tourCategory',
       title: 'Movie Category',
-      content: (
-        <div>
-          To navigate around different movie categories, you can simply click the navigation button or swipe <span className={styles.swipeUpIcon} /> up and down on your awesome keyboard
-        </div>
-      ),
+      content: `
+      To navigate around different movie categories, you can simply click the navigation 
+      button or press ↑ up and ↓ down on your awesome keyboard`,
       placement: 'right',
       disableBeacon: true,
-      styles: {
-        tooltip: {
-          maxWidth: '100%',
-        },
-      },
+      disableOverlayClicks: true,
     },
     {
       target: '.tourSlide',
       title: 'Highlighted Movies',
-      content: (
-        <div>
-          {' '}
-          You can browse through our top movies in each category with gentle click on the arrow buttons or using keyboards and swipe <span className={styles.swipeNextIcon} /> right and left
-        </div>
-      ),
+      content: `
+      You can browse through our top movies in each category with gentle click on the arrow buttons 
+      or using keyboards and toggle → right and ← left`,
       placement: 'top',
       disableBeacon: true,
+      disableOverlayClicks: true,
     },
     {
       target: '.tourLibrary',
@@ -90,20 +79,25 @@ export const tourSteps = {
       content: 'You can click this icon to view all movie list per category',
       placement: 'bottom',
       disableBeacon: true,
+      disableOverlayClicks: true,
     },
     // {
     //   target: '.tourMovieDiscover',
     //   title: 'Discover Our Movie',
     //   content: 'Click this button to discover our awesome list of movies',
     //   placement: 'top',
+    //   spotlightPadding: 0,
     //   disableBeacon: true,
+    //   disableOverlayClicks: true,
     // },
     {
       target: '.tourMovieDetail',
       title: 'View Movie Detail',
       content: 'Click this button to watch movie and view movie detail: synopsis, testimonial, cast, and trailer',
       placement: 'top',
+      spotlightPadding: 0,
       disableBeacon: true,
+      disableOverlayClicks: true,
       locale: { last: 'Finish' },
     },
   ],
