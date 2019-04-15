9cba804f46f743dc8db131ac5f73c5292d1bdd7e
ayu <ayu@ayus-MacBook-Pro.local>
2019-04-15 11:58:34 +0700

    hide new header for sport

--- src/routes/movie-library/movielibrary/Libheader.js
+++ src/routes/movie-library/movielibrary/Libheader.js
@@ -88,6 +88,7 @@ class Libheader extends Component {
           title={cardTitle}
           isLibrary
           isLibraryCopy
+          menuDropDownHeader
           stickyOff
           {...this.props}
           handleMenuToggleClick={this.handleMenuToggleClick}
