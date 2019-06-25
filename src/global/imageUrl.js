52f79fbd63750b657dca7eeb76bc43d705d8a664
irene <irene.felita@supersoccer.co.id>
2019-06-25 18:53:59 +0700

    update cdn link to view all images #41

--- src/global/imageUrl.js
+++ src/global/imageUrl.js
@@ -45,5 +45,5 @@ export const google = `${imgBaseUrl}/google.png`
 
 export const defaultImgClub = `${imgBaseUrl}/defaultImage.svg`
 
-export const viewAllMovieBg = `${imgBaseUrl}/background_darker.png`
-export const viewAllMovieImg = `${imgBaseUrl}/viewallmovie.png`
+export const viewAllMovieBg = `${endpoints.asset}/background_darker.png`
+export const viewAllMovieImg = `${endpoints.asset}/viewallmovie.png`
