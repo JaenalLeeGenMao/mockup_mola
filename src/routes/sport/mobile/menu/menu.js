c380272efe452e647c333436ade315cd29c5434a
Elkana Lumbantobing <elkana.ltobing@gmail.com>
2019-07-16 16:46:30 +0700

    handle isDark banner-sport

--- src/routes/sport/mobile/menu/menu.js
+++ src/routes/sport/mobile/menu/menu.js
@@ -6,15 +6,12 @@ import LazyLoad from '@components/common/Lazyload'
 import horizontalStyles from './menu-horizontal.css'
 import verticalStyles from './menu-vertical.css'
 
-const SportMenu = ({ className = '', playlists, activeIndex = 0, isGray = false, isDark = 0, type = 'vertical' }) => {
+const SportMenu = ({ className = '', playlists, activeIndex = 0, isDark = 0, type = 'vertical', isGray = 0 }) => {
   const styles = type === 'vertical' ? verticalStyles : horizontalStyles
   return (
     <LazyLoad containerClassName={[styles.menu, className].join(' ')}>
       {playlists.map(({ id }, index) => (
-        <div
-          key={id}
-          className={`${styles.menu__dots} ${isGray ? styles.gray : ''} ${isDark ? styles.black : styles.white} ${index === activeIndex ? styles.menu__active : ''}`}
-        />
+        <div key={id} className={`${styles.menu__dots} ${isDark ? styles.black : styles.white} ${isGray && styles.gray} ${index === activeIndex ? styles.menu__active : ''}`} />
       ))}
       {/* {type == 'horizontal' && <div className={`${styles.menu__dots} ${isDark ? styles.black : styles.white} ${activeIndex + 1 === playlists.length ? styles.menu__bot_active : ''}`} />} */}
     </LazyLoad>
