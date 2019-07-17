40e8d6a1ca4c2e5dffd391cf73b20b2d9b214d84
irene <irene.felita@supersoccer.co.id>
2019-07-17 21:39:56 +0700

    update on enter and space banner home

--- src/routes/home/desktop/home.js
+++ src/routes/home/desktop/home.js
@@ -203,7 +203,7 @@ class Home extends Component {
       this.prevTouchY = event.screenY
     }
 
-    var handleClick = function (e) {
+    var handleClick = function(e) {
       var target = e.target
       var isPlaylist = target.parentElement.getElementsByClassName('is-home-playlist').length
 
@@ -349,7 +349,7 @@ class Home extends Component {
     $.data(
       that,
       'scrollCheck',
-      setTimeout(function () {
+      setTimeout(function() {
         /* Determine the direction of the scroll (< 0 → up, > 0 → down). */
         var delta = (event.deltaY || -event.wheelDelta || event.detail) >> 10 || 1
 
@@ -389,7 +389,9 @@ class Home extends Component {
           this.handleScrollToIndex(scrollIndex + 1)
           break
         case 13 /* enter */:
-          if (activeSlide.id) {
+          if (scrollIndex == 0) {
+            window.location.href = activeSlide.link
+          } else if (activeSlide.id) {
             if (isMovie(activeSlide.contentType)) {
               window.location.href = `/movie-detail/${activeSlide.id}`
             } else {
@@ -402,7 +404,9 @@ class Home extends Component {
           }
           break
         case 32 /* space */:
-          if (activeSlide.id) {
+          if (scrollIndex == 0) {
+            window.location.href = activeSlide.link
+          } else if (activeSlide.id) {
             if (isMovie(activeSlide.contentType)) {
               window.location.href = `/movie-detail/${activeSlide.id}`
             } else {
@@ -473,7 +477,7 @@ class Home extends Component {
   handleColorChange = (index, swipeIndex = 0) => {
     // console.log('MASUK SINI swipeIndex????', swipeIndex)
     const that = this
-    setTimeout(function () {
+    setTimeout(function() {
       // that.props.onUpdatePlaylist(activePlaylist.id)
       const activeSlick = document.querySelector(`.slick-active .${contentStyles.content__container} .slick-active .grid-slick`),
         { videos, sliderRefs } = that.state
@@ -545,11 +549,11 @@ class Home extends Component {
 
   render() {
     const {
-      playlists,
-      playlists: { meta: { status: playlistStatus = 'loading', error: playlistError = '' } },
-      videos,
-      videos: { meta: { status: videoStatus = 'loading', error: videoError = '' } },
-    } = this.props.home,
+        playlists,
+        playlists: { meta: { status: playlistStatus = 'loading', error: playlistError = '' } },
+        videos,
+        videos: { meta: { status: videoStatus = 'loading', error: videoError = '' } },
+      } = this.props.home,
       { locale, isDark, startGuide, steps, playlistSuccess, stepIndex, sliderRefs, scrollIndex, swipeIndex, activeSlide, activeSlideDots } = this.state,
       settings = {
         ...SETTINGS_VERTICAL,
@@ -624,8 +628,8 @@ class Home extends Component {
                   {scrollIndex == 0 && activeSlideDots && activeSlideDots.length > 1 ? (
                     this.renderMenuBanner(activeSlide, this.state.playlists.data, scrollIndex, this.handleScrollToIndex)
                   ) : (
-                      <HomeMenu playlists={this.state.playlists.data} activeIndex={scrollIndex} isDark={0} onClick={this.handleScrollToIndex} />
-                    )}
+                    <HomeMenu playlists={this.state.playlists.data} activeIndex={scrollIndex} isDark={0} onClick={this.handleScrollToIndex} />
+                  )}
                 </div>
                 {scrollIndex != 0 &&
                   activeSlide &&
@@ -656,25 +660,26 @@ class Home extends Component {
                   swipeIndex + 1 === videos.data[scrollIndex].data.length && (
                     <LazyLoad containerClassName={styles.view_all_movie_container}>
                       <picture>
-                        {this.state.playlists.data[scrollIndex].iconUrl ?
+                        {this.state.playlists.data[scrollIndex].iconUrl ? (
                           <>
                             <source srcSet={this.state.playlists.data[scrollIndex].iconWebp} type="image/webp" />
                             <source srcSet={this.state.playlists.data[scrollIndex].iconUrl} type="image/jpeg" />
                             <img src={this.state.playlists.data[scrollIndex].iconUrl} />
                           </>
-                          :
+                        ) : (
                           <>
                             <source srcSet={viewAllMovieImgWebp} type="image/webp" />
                             <source srcSet={viewAllMovieImg} type="image/jpeg" />
                             <img src={viewAllMovieImg} />
                           </>
-                        }
+                        )}
                       </picture>
                       <a href={`/movie-library/${libraryId}`}>
                         <span>
                           {locale['view_all_movie']}
                           <br /> {this.state.playlists.data[scrollIndex].title.toUpperCase() + ' '}
-                          {locale['other']}</span>
+                          {locale['other']}
+                        </span>
                         <i />
                       </a>
                     </LazyLoad>
@@ -686,8 +691,8 @@ class Home extends Component {
                     {scrollIndex == 0 && activeSlideDots && activeSlideDots.length > 1 ? (
                       this.renderMenuBanner(activeSlide, activeSlideDots, swipeIndex, this.handleNextPrevSlide, 'horizontal')
                     ) : (
-                        <HomeMenu playlists={activeSlideDots} activeIndex={swipeIndex} isDark={0} onClick={this.handleNextPrevSlide} type="horizontal" />
-                      )}
+                      <HomeMenu playlists={activeSlideDots} activeIndex={swipeIndex} isDark={0} onClick={this.handleNextPrevSlide} type="horizontal" />
+                    )}
                   </div>
                 </div>
                 <Slider
