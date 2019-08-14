fffff7e8711bb9521b76354fe23fae3eeaba3670
irene <irene.felita@supersoccer.co.id>
2019-08-14 14:06:18 +0700

    add crossorigin on ecternal script, update to v15

--- src/components/Html.js
+++ src/components/Html.js
@@ -307,7 +307,7 @@ applicationID:${config.env === 'production' ? '172046541' : '171080482'},sa:1}`,
                 },'auto');ga('send','pageview')`,
             }}
           />
-          <script type="text/javascript" src="https://service.force.com/embeddedservice/5.0/esw.min.js" />
+          <script type="text/javascript" src="https://service.force.com/embeddedservice/5.0/esw.min.js" crossOrigin />
           <script
             dangerouslySetInnerHTML={{
               __html: `var initESW = function(gslbBaseURL) {
@@ -362,7 +362,7 @@ applicationID:${config.env === 'production' ? '172046541' : '171080482'},sa:1}`,
             }}
           />
 
-          <script src="https://www.google-analytics.com/analytics.js" async="" defer="" />
+          <script src="https://www.google-analytics.com/analytics.js" async="" defer="" crossOrigin />
         </body>
       </html>
     )
