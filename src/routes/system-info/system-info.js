707c9fd826581092d57100d99e21cb9fbd9cc156
sabiqsa <sabiqsa@gmail.com>
2019-08-16 14:20:26 +0700

    update v16

--- src/routes/system-info/system-info.js
+++ src/routes/system-info/system-info.js
@@ -39,7 +39,7 @@ class SystemInfo extends Component {
                 <div>IP {info.ip}</div>
               </>
             )}
-            <div>Version 1.0.15</div>
+            <div>Version 1.0.16</div>
           </div>
         </div>
       </div>
