643b220817409badc0e4dd6e2042c2a1fdad5430
irene <irene.felita@supersoccer.co.id>
2019-08-27 20:03:17 +0700

    v20

--- src/routes/system-info/system-info.js
+++ src/routes/system-info/system-info.js
@@ -39,7 +39,7 @@ class SystemInfo extends Component {
                 <div>IP {info.ip}</div>
               </>
             )}
-            <div>Version 1.0.19</div>
+            <div>Version 1.0.20</div>
           </div>
         </div>
       </div>
