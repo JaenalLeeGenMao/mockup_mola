ebc8f37882709cae6245a2e4a1a4b36351f4864b
ayu <ayu@ayus-MacBook-Pro.local>
2019-10-03 13:30:25 +0700

    Update League Filter New Design [Matches Page] #72

--- src/components/HorizontalPlaylist/HorizontalPlaylist.js
+++ src/components/HorizontalPlaylist/HorizontalPlaylist.js
@@ -5,6 +5,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles'
 // import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap'
 
 import s from './HorizontalPlaylist.css'
+import ReactTooltip from 'react-tooltip'
 
 class HorizontalPlaylist extends Component {
   static propTypes = {
@@ -16,30 +17,85 @@ class HorizontalPlaylist extends Component {
     allButtonOn: PropTypes.bool,
   }
 
-  handleScroll = () => {
+  constructor(props) {
+    super(props)
+    this.isFarLeftReach = true
+    this.isFarRightReach = false
+  }
+
+  componentDidMount() {
+    //function hover show and hide button
+    var leagueList = document.getElementById('filLeague')
+    var defEelementValue = document.getElementById('league__wrapper_scroller')
+
+    leagueList.onmouseover = () => {
+      document.getElementById('prev').style.visibility = this.isFarLeftReach ? 'hidden' : 'visible'
+      document.getElementById('next').style.visibility = this.isFarRightReach ? 'hidden' : 'visible'
+      // console.log('see result isfarright', this.isFarRightReach)
+      // console.dir(defEelementValue)
+
+      if (defEelementValue.offsetWidth > defEelementValue.scrollWidth) {
+        this.isFarRightReach = false
+      }
+
+      if (defEelementValue.scrollWidth == defEelementValue.offsetWidth) {
+        this.isFarRightReach = true
+        document.getElementById('next').style.visibility = 'hidden'
+      }
+    }
+    leagueList.onmouseout = hide
+    function hide() {
+      document.getElementById('prev').style.visibility = 'hidden'
+      document.getElementById('next').style.visibility = 'hidden'
+    }
+
+    //function for scroll all category horizontall scroll
     var buttonNext = document.getElementById('next')
     buttonNext.addEventListener('click', function() {
       var containerNext = document.getElementById('league__wrapper_scroller')
-      sideScroll(containerNext, 'right', 25, 100, 10)
+      sideScroll(containerNext, 'right', 25, 120, 10)
     })
 
     var buttonPrev = document.getElementById('prev')
     buttonPrev.addEventListener('click', function() {
       var containerPrev = document.getElementById('league__wrapper_scroller')
-      sideScroll(containerPrev, 'left', 25, 100, 10)
+      sideScroll(containerPrev, 'left', 25, 120, 10)
     })
 
-    function sideScroll(element, direction, speed, distance, step) {
+    //calculation if scrollleft reach endof scroll and arrow will hidden or not reach endof scroll arrow still visible for click next data
+    const sideScroll = (element, direction, speed, distance, step) => {
       let scrollAmount = 0
-
-      var slideTimer = setInterval(function() {
+      // console.log('checking start')
+      var slideTimer = setInterval(() => {
         if (direction == 'left') {
           element.scrollLeft -= step
+
+          if (element.scrollLeft == 0) {
+            this.isFarLeftReach = true
+            document.getElementById('prev').style.visibility = 'hidden'
+          } else {
+            this.isFarLeftReach = false
+          }
+          if (element.scrollLeft + element.offsetWidth < element.scrollWidth) {
+            this.isFarRightReach = false
+          }
         } else {
           element.scrollLeft += step
+          leagueList = document.getElementById('filLeague')
+          defEelementValue = document.getElementById('league__wrapper_scroller')
+
+          // console.dir(element)
+          if (element.scrollLeft + element.offsetWidth >= element.scrollWidth) {
+            this.isFarRightReach = true
+            document.getElementById('next').style.visibility = 'hidden'
+          } else {
+            this.isFarRightReach = false
+          }
+          if (element.scrollLeft != 0) {
+            this.isFarLeftReach = false
+          }
         }
         scrollAmount += step
-
         if (scrollAmount >= distance) {
           window.clearInterval(slideTimer)
         }
@@ -75,7 +131,10 @@ class HorizontalPlaylist extends Component {
                       {genre.thumbnails &&
                         loadedThumbnail && (
                           <img
-                            className={s.img}
+                            // className={s.img}
+                            className={`${s.img} ${genre.id == filterByLeague ? s.selected_img : s.img}`}
+                            data-tip={genre.title}
+                            data-for={genre.title}
                             src={genre.thumbnails}
                             onLoad={() => {
                               loadedThumbnail(true)
@@ -83,14 +142,19 @@ class HorizontalPlaylist extends Component {
                           />
                         )}
                     </span>
-                    <span
-                      value={genre.id}
-                      className={filterByLeague == genre.id ? s.selected_playlist : s.playlist__container}
-                    >
-                      {genre.title}
-                    </span>
                   </div>
                 )}
+                <div className={s.tooltipContainer}>
+                  <ReactTooltip
+                    id={genre.title}
+                    place="bottom"
+                    aria-haspopup="true"
+                    effect="solid"
+                    className={s.blueTooltip}
+                  >
+                    <p>{genre.title}</p>
+                  </ReactTooltip>
+                </div>
               </div>
             </>
           )
@@ -105,46 +169,34 @@ class HorizontalPlaylist extends Component {
 
     return (
       <div className={s.match_ligaType}>
-        <span className={`${s.filLeague} tourPlaylist`}>
-          {allCat.map(dt => {
-            return (
-              <>
-                <div
-                  key={dt}
-                  className={`${s.playlist_all} ${
-                    dt.id == filterByLeague ? s.selected_playlist : s.playlist__container
-                  }`}
-                  onClick={() => {
-                    handleCategoryFilter('All')
-                  }}
-                >
-                  All
-                </div>
-              </>
-            )
-          })}
+        <span className={`${s.filLeague} tourPlaylist`} id="filLeague">
           <div className={s.btnPrevContainer}>
-            <span
-              className={s.btnPrevHori}
-              id="prev"
-              onClick={() => {
-                this.handleScroll('prev')
-              }}
-            />
+            <div className={s.btnPrevHori} id="prev" />
           </div>
-          <div className={s.league__wrapper}>
+          <div className={s.league__wrapper} id="league__wrapper">
             <div className={s.league__wrapper_scroller} id="league__wrapper_scroller">
+              {allCat.map(dt => {
+                return (
+                  <>
+                    <div
+                      key={dt}
+                      className={`${s.playlist_all} ${
+                        dt.id == filterByLeague ? s.selected_playlist : s.playlist__container
+                      }`}
+                      onClick={() => {
+                        handleCategoryFilter('All')
+                      }}
+                    >
+                      All
+                    </div>
+                  </>
+                )
+              })}
               {this.categoryFilterLigaType()}
             </div>
           </div>
           <div className={s.btnNextContainer}>
-            <span
-              className={s.btnNextHori}
-              id="next"
-              onClick={() => {
-                this.handleScroll('next')
-              }}
-            />
+            <div className={s.btnNextHori} id="next" />
           </div>
         </span>
       </div>
