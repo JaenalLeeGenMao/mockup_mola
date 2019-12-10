44f95cb411d3c8377f12f9f7f634550665665fc5
Risal Falah <risal.falah@gmail.com>
2019-12-10 16:14:12 +0700

    checking push_docker.sh

--- .gitlab/push_docker.sh
+++ .gitlab/push_docker.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # Set image name
-REPOSITORY_ADDRESS="${REPOSITORY_HOST}/${GCLOUD_PROJECT_ID}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}-${CI_COMMIT_REF_NAME}"
-IMAGE_NAME="${REPOSITORY_ADDRESS,,}:${CI_PIPELINE_IID}"
+REPOSITORY_ADDRESS="${REPOSITORY_HOST}/${GCLOUD_PROJECT_ID}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}"
+IMAGE_NAME="${REPOSITORY_ADDRESS}:${CI_PIPELINE_IID}"
 
 # Auth gcloud
 gcloud auth configure-docker --quiet
@@ -10,8 +10,12 @@ gcloud auth configure-docker --quiet
 # Build Docker!
 docker build --tag $IMAGE_NAME .
 
-gsutil -m -h "Cache-Control:public,max-age=31556952" cp -r build/public/assets/* ${GCS_PATH}/assets/
-gsutil -m -h "Cache-Control:public,max-age=31556952" cp -r src/global/assets-global/* ${GCS_PATH}/assets-global/
+# FOR TESTING
+echo $IMAGE_NAME
 
 # Push docker image
 docker push $IMAGE_NAME
+
+# Push Assets to CDN
+gsutil -m -h -q "Cache-Control:public,max-age=31556952" cp -r build/public/assets/* ${GCS_PATH}/assets/
+gsutil -m -h -q "Cache-Control:public,max-age=31556952" cp -r src/global/assets-global/* ${GCS_PATH}/assets-global/
