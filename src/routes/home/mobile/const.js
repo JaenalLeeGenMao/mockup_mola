0ce3cd47003af42a3bd104f2aa7b277e3fd52c1b
angrha <angrha@gmail.com>
2020-02-03 15:22:32 +0700

    home mobile change to featured, recommit #103

--- src/routes/home/mobile/const.js
+++ src/routes/home/mobile/const.js
@@ -89,3 +89,61 @@ export const tourSteps = {
     },
   ],
 }
+
+export const contentTypeList = {
+  vod: {
+    id: 1,
+    slideToShow: 7.5,
+    slideToScroll: 2.8,
+  },
+  linear: {
+    id: 2,
+    slideToShow: 5.5,
+    slideToScroll: 1.6,
+  },
+  live: {
+    id: 3,
+    slideToShow: 5.5,
+    slideToScroll: 1.6,
+  },
+  replay: {
+    id: 4,
+    slideToShow: 5.5,
+    slideToScroll: 1.6,
+  },
+  mixed: {
+    id: 5,
+    slideToShow: 5.5,
+    slideToScroll: 1.6,
+  },
+  movie: {
+    id: 7,
+    slideToShow: 7.25,
+    slideToScroll: 2.8,
+  },
+  trailers: {
+    id: 8,
+    slideToShow: 5.5,
+    slideToScroll: 1.6,
+  },
+  'mola-featured': {
+    id: 9,
+    slideToShow: 4.5,
+    slideToScroll: 1.3,
+  },
+  'mola-categories': {
+    id: 10,
+    slideToShow: 6.25,
+    slideToScroll: 1.6,
+  },
+  articles: {
+    id: 'articles',
+    slideToShow: 3.5,
+    slideToScroll: 1,
+  },
+  banners: {
+    id: 'banners',
+    slideToShow: 2,
+    slideToScroll: 1,
+  },
+}
